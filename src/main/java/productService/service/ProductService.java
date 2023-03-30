2a32c1eb968ddb634b9405e2a2ba3f40f0d29130
Abir <abirhossain9@iut-dhaka.edu>
2023-03-30 18:01:20 +0600

    Adds findAll method in product service

--- src/main/java/productService/service/ProductService.java
+++ src/main/java/productService/service/ProductService.java
@@ -1,45 +1,26 @@
 package productService.service;
 
-import productService.dto.ProductRequest;
-import productService.dto.ProductResponse;
 import productService.model.Product;
 import productService.repository.ProductRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
-@Slf4j
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
 
-    private final ProductRepository productRepository;
-
-    public void createProduct(ProductRequest productRequest) {
-        Product product = Product.builder()
-                .name(productRequest.getName())
-                .description(productRequest.getDescription())
-                .price(productRequest.getPrice())
-                .build();
-
-        productRepository.save(product);
-        log.info("Product {} is saved", product.getId());
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
     }
 
-    public List<ProductResponse> getAllProducts() {
-        List<Product> products = productRepository.findAll();
-
-        return products.stream().map(this::mapToProductResponse).toList();
+    public Product findProductById(String userId) {
+        return productRepository.findProductById(userId);
     }
 
-    private ProductResponse mapToProductResponse(Product product) {
-        return ProductResponse.builder()
-                .id(product.getId())
-                .name(product.getName())
-                .description(product.getDescription())
-                .price(product.getPrice())
-                .build();
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
     }
 }
